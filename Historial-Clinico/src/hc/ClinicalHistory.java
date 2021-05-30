b469cd9838ab8d938d6b5dfda401150b95a1bd86
Karen Garcia <karengarciafranco0811@gmail.com>
2021-05-29 23:34:34 -0500

    Create void Display

--- Historial-Clinico/src/hc/ClinicalHistory.java
+++ Historial-Clinico/src/hc/ClinicalHistory.java
@@ -1,3 +1,4 @@
+
 public class ClinicalHistory {
     /**
 	 * Attributes of ClinicalHistory
@@ -59,6 +60,17 @@ public void setObservations(String observations) {
 this.observations = observations;
 }
 
-
+/**
+* Displays by console all the data of the ClinicalHistory
+*/
+public void Display() {
+    System.out.println("\n\t---------------");
+    System.out.println("History Code: " + code);
+    System.out.println("Date: " + date);
+    System.out.println("ID Pacient: " + idPacient);
+    System.out.println("ID Doctor: " + idDoctor);
+    System.out.println("Observations:\n" + observations);
+    System.out.println("\t---------------");
+    }
 }
 
