7ae2d23fefd0408c5b8cb81755331215da0c5acb
edmundoL <ed3696@gmail.com>
2021-05-29 23:19:47 -0700

    Hola

---
+++ Historial-Clinico/src/hc/Doctor.java
@@ -0,0 +1,55 @@
+package hc;
+
+public class Doctor {
+    private String id;
+    private String name;
+    private String lastname;
+    private String specialization;
+    public Doctor(String id, String name, String lastname, String specialization) {
+		this.id = id;
+		this.name = name;
+		this.lastname = lastname;
+		this.specialization = specialization;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getlLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+	
+	public void mostrar() {
+		System.out.println("\n\t---------------");
+		System.out.println("ID Medico: " + id);
+		System.out.println("Nombre: " + name);
+		System.out.println("Apellidos: " + lastname);
+		System.out.println("Especialidad: " + specialization);
+		System.out.println("\t---------------");
+	}
+}
