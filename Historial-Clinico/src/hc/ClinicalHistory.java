38617b99cbf10698a93d2ff59378541c136fb4c2
Karen Garcia <karengarciafranco0811@gmail.com>
2021-05-29 23:24:06 -0500

    definion attributes

---
+++ Historial-Clinico/src/hc/ClinicalHistory.java
@@ -0,0 +1,12 @@
+public class ClinicalHistory {
+    /**
+	 * Attributes of ClinicalHistory
+	 */
+    private String code;
+	private String date;
+	private String idPacient;
+	private String idDoctor;
+	private String observations;
+
+}
+
