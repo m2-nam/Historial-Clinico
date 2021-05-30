136850049d4c974dc0e2602298749d8fae467ee7
Karen Garcia <karengarciafranco0811@gmail.com>
2021-05-29 23:30:38 -0500

    add getters and setters of the attributes

--- Historial-Clinico/src/hc/ClinicalHistory.java
+++ Historial-Clinico/src/hc/ClinicalHistory.java
@@ -7,6 +7,58 @@ public class ClinicalHistory {
 	private String idPacient;
 	private String idDoctor;
 	private String observations;
+ 
+    /**
+	 * Definition Constructor
+	 */
+	public ClinicalHistory(String code, String date, String idPacient,
+    String idDoctor, String observations) {
+this.code = code;
+this.date = date;
+this.idPacient = idPacient;
+this.idDoctor = idDoctor;
+this.observations = observations;
+}
+
+/**
+* Definition Get of Attributes
+*/
+public String getCode() {
+return code;
+}
+public String getDate() {
+return date;
+}
+public String getIdPacient() {
+return idPacient;
+}
+public String getIdDoctor() {
+return idDoctor;
+}
+public String getObservations() {
+return observations;
+}
+
+
+/**
+* Definition Set of Attributes
+*/
+public void setCode(String code) {
+this.code = code;
+}
+public void setDate(String date) {
+this.date = date;
+}
+public void setIdPacient(String idPacient) {
+this.idPacient = idPacient;
+}
+public void setIdDoctor(String idDoctor) {
+this.idDoctor = idDoctor;
+}
+public void setObservations(String observations) {
+this.observations = observations;
+}
+
 
 }
 
