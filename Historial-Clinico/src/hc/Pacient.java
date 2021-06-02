7abc02649e19c62d6106c35cadc86dfedd5b4188
Yesenia Medina <yesekcl@gmail.com>
2021-06-01 21:07:25 -0500

    Adding Basic Pacient Class

--- Historial-Clinico/src/hc/Pacient.java
+++ Historial-Clinico/src/hc/Pacient.java
@@ -1,3 +1,50 @@
 public class Pacient {
-    
+    private int dni;
+    private String name;
+    private String lastName;
+    private char sex;
+    private int age;
+
+    public Pacient(int dni, String name, String lastName, char sex, int age) {
+        this.dni = dni;
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+    public int getDni() {
+        return dni;
+    }
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public char getSex() {
+        return sex;
+    }
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        String text = "PACIENTE \nDNI: " + dni + "\nName: " + name + "\nLastName: " + lastName + "\nSex: " + sex + "\nAge: " + age;
+        return text;
+    }
 }
--- Historial-Clinico/src/hc/Test.java
+++ Historial-Clinico/src/hc/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("");
+        Pacient P1 = new Pacient(101010, "Elizabeth", "Jones", 'F', 43);
+        System.out.println(P1);
     }
 }
